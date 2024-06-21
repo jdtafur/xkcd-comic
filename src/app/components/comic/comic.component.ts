37f09b9a1eb5bdffef1fc3847de38fbf6f548d17
jdtafurOctan <jdtafur@octansit.com>
2024-06-21 18:17:03 -0500

    get random comic
    map transcript data
    
    custom comic view component

--- src/app/components/comic/comic.component.ts
+++ src/app/components/comic/comic.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ComicService} from "../../core/services/comic.service";
 import {HttpClientModule} from "@angular/common/http";
-import {JsonPipe} from "@angular/common";
+import {JsonPipe, NgTemplateOutlet} from "@angular/common";
 import {Comic} from "../../core/interfaces/comic.interface";
+
+const RANGE = {
+  min: 1,
+  max: 2948,
+}
+
 
 @Component({
   selector: 'app-comic',
   standalone: true,
-  imports: [HttpClientModule, JsonPipe],
+  imports: [HttpClientModule, JsonPipe, NgTemplateOutlet],
   providers: [ComicService],
   templateUrl: './comic.component.html',
   styleUrl: './comic.component.scss'
@@ -22,6 +28,21 @@ export class ComicComponent {
   constructor(private _comicService: ComicService) {
     this._comicService.getCurrent().subscribe((comic: Comic) => {
       this.comicCurrent = comic;
-    })
+    });
+  }
+
+  getComicByRandomNumber(): void {
+    const randomNumberComic: number = Math.floor(Math.random() * (RANGE.max - RANGE.min + 1)) + RANGE.min;
+    this._comicService.getByNumber(randomNumberComic).subscribe((comic: Comic) => {
+      this.comicCurrent = comic;
+    });
+  }
+
+  getTranscript(transcript: string): string[] {
+    return transcript.split('\n\n')
+  }
+
+  getSubTranscript(transcript: string): string[] {
+    return transcript.split('\n')
   }
 }
